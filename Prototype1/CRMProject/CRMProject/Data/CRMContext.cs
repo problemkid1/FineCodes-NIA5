fed9965838e949c24c76d18052f28e31a984a6c2
Shadab Hasan <cyrushasan@yahoo.com>
2025-01-19 22:38:04 -0500

    Implemented Cascade Delete and unique constraint n f

--- Prototype1/CRMProject/CRMProject/Data/CRMContext.cs
+++ Prototype1/CRMProject/CRMProject/Data/CRMContext.cs
@@ -25,6 +25,101 @@ namespace CRMProject.Data
         public DbSet<Opportunity> Opportunities { get; set; }
 
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Prevent Cascade Delete from Contact to ContactEmail
+            modelBuilder.Entity<ContactEmail>()
+                .HasOne(ce => ce.Contact)
+                .WithMany(c => c.ContactEmails)
+                .HasForeignKey(ce => ce.ContactID)
+                .OnDelete(DeleteBehavior.Restrict);
 
+            //Prevent Cascade Delete from Industry to MemberIndustry
+            modelBuilder.Entity<MemberIndustry>()
+                .HasOne(mi => mi.Industry)
+                .WithMany(i => i.MemberIndustries)
+                .HasForeignKey(mi => mi.IndustryID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Prevent Cascade Delete from Member to MemberIndustry
+            modelBuilder.Entity<MemberIndustry>()
+                .HasOne(mi => mi.Member)
+                .WithMany(m => m.MemberIndustries)
+                .HasForeignKey(mi => mi.MemberID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Prevent Cascade Delete from Member to MemberContact
+            modelBuilder.Entity<MemberContact>()
+                .HasOne(mc => mc.Member)
+                .WithMany(m => m.MemberContacts)
+                .HasForeignKey(mc => mc.MemberID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Prevent Cascade Delete from Contact to MemberContact
+            modelBuilder.Entity<MemberContact>()
+                .HasOne(mc => mc.Contact)
+                .WithMany(c => c.MemberContacts)
+                .HasForeignKey(mc => mc.ContactID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Prevent Cascade Delete from MembershipType to MemberMembershipType
+            modelBuilder.Entity<MemberMembershipType>()
+                .HasOne(mmt => mmt.MembershipType)
+                .WithMany(mt => mt.MemberMembershipTypes)
+                .HasForeignKey(mmt => mmt.MembershipTypeID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Prevent Cascade Delete from Member to MemberMembershipType
+            modelBuilder.Entity<MemberMembershipType>()
+                .HasOne(mmt => mmt.Member)
+                .WithMany(m => m.MemberMembershipTypes)
+                .HasForeignKey(mmt => mmt.MemberID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Prevent Cascade Delete from Member to Cancellation
+            modelBuilder.Entity<Cancellation>()
+                .HasOne(c => c.Member)
+                .WithMany(m => m.Cancellations)
+                .HasForeignKey(c => c.MemberID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Unique Index for Contacs email
+            modelBuilder.Entity<ContactEmail>()
+                .HasIndex(ce => ce.EmailAddress)
+                .IsUnique();
+
+            //Unique Index for Members name
+            modelBuilder.Entity<Member>()
+                .HasIndex(m => new { m.MemberName, m.MemberSize, m.MemberStatus })
+                .IsUnique();
+
+            //Unique Index for Opportunity Name
+            modelBuilder.Entity<Opportunity>()
+                .HasIndex(o => o.OpportunityName)
+                .IsUnique();
+
+            //Many-to-Many MemberIndustry
+            modelBuilder.Entity<MemberIndustry>()
+                .HasKey(mi => new { mi.MemberID, mi.IndustryID });
+
+            //Many-to-Many MemberMembershipType
+            modelBuilder.Entity<MemberMembershipType>()
+                .HasKey(mmt => new { mmt.MemberID, mmt.MembershipTypeID });
+
+            //Unique Index for Industry Name
+            modelBuilder.Entity<Industry>()
+                .HasIndex(i => i.IndustryName)
+                .IsUnique();
+
+            //Unique Index for MembershipTypeName
+            modelBuilder.Entity<MembershipType>()
+                .HasIndex(mt => mt.MembershipTypeName)
+                .IsUnique();
+
+            //Unique Index for OpportunityAccount
+            modelBuilder.Entity<Opportunity>()
+                .HasIndex(o => o.OpportunityAccount)
+                .IsUnique();
+        }
     }
 }
