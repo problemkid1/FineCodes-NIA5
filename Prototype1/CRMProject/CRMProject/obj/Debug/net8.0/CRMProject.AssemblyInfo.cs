e21443885f81e5942a72c2e0d168b2030d888f4c
Shadab Hasan <cyrushasan@yahoo.com>
2025-01-28 17:37:10 -0500

    updated contacts format

--- Prototype1/CRMProject/CRMProject/obj/Debug/net8.0/CRMProject.AssemblyInfo.cs
+++ Prototype1/CRMProject/CRMProject/obj/Debug/net8.0/CRMProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRMProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6214d353feb3db13c6cdcef38bf2a7ddcec6217f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3656a214afd5751f1bb46ba0ed5465ed7dd68d65")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRMProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRMProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
