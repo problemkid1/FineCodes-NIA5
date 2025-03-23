29ce2cfa98b67c9cc6f9f4f6a8cbe659162b63fe
Geisy <geisyannesoares@gmail.com>
2025-03-23 17:39:15 -0400

    Adding Shadab's work..Annual and LAMContact

---
+++ Prototype2/CRMProject/CRMProject/Models/LAMContact.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRMProject.Models
+{
+    public class LAMContact
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Municipality is required.")]
+        [Display(Name = "Municipality")]
+        [MaxLength(100, ErrorMessage = "Municipality cannot be more than 100 characters long.")]
+        public string Municipality { get; set; } = "";
+
+        [Required(ErrorMessage = "Position is required.")]
+        [Display(Name = "Position")]
+        [MaxLength(100, ErrorMessage = "Position cannot be more than 100 characters long.")]
+        public string Position { get; set; } = "";
+
+        [Display(Name = "Notes")]
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500, ErrorMessage = "Notes cannot be more than 500 characters long.")]
+        public string? Notes { get; set; }
+
+        [Required(ErrorMessage = "You must select a contact.")]
+        [Display(Name = "Contact")]
+        public int ContactID { get; set; }
+
+        public Contact? Contact { get; set; }
+    }
+}
